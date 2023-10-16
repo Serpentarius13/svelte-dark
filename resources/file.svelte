7b8e772c6100b5f462eed42e5814a61c09ae76cf
Serpentarius13 <serpentarium13@mail.ru>
2023-10-16 03:06:01 +0300

    v1

---
+++ resources/file.svelte
@@ -0,0 +1,22 @@
+
+
+<script lang="ts">
+
+
+
+</script>
+<svelte:fragment>
+
+</svelte:fragment>
+
+<svelte:head>
+
+</svelte:head>
+<div>
+
+</div>
+
+
+<div>
+
+</div>
---
+++ оы.js
@@ -0,0 +1,8 @@
+class Red {
+
+}
+
+function functionless() {
+
+}
+
